02a6682daf6930a56db6e03773a85ffc8431d11c
KdotA <kenan@akinci.io>
2022-01-16 15:25:10 +0100

    Adding components and using the states

--- monsterz/src/App.js
+++ monsterz/src/App.js
@@ -1,26 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      string: 'Hello Ken!',
+      monsters: [],
+      searchField: '',
     };
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users })); //this.setState() is also Async
+  }
+
+  //This will be called everytime when the state changes
   render() {
+    //console.log(this.state.monsters); //--> Shows the this on console. At this point, it's the App class.
+    const { monsters, searchField } = this.state; // equals to const monsters = this.state.monsters etc.
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: 'Goodbye' })}>
-            Click!
-          </button>
-        </header>
+        <SearchBox
+          placeholder="Search monsters"
+          handleChange={e => this.setState({ searchField: e.target.value })}
+          //everytime a change is triggered on the input element, state is changing.
+          //since the setState() is Async, we can pass a second argument.
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
---
+++ monsterz/src/components/search-box/search-box.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './search-box.styles.css';
+
+export const SearchBox = ({ placeholder, handleChange }) => (
+  <input
+    className="search"
+    type="search"
+    placeholder={placeholder}
+    onChange={handleChange}
+  />
+);
+
+/* There is no state inside this component because the data flows in one way.
+There is no way that the CardList or other components has an access when the state changes here.
+Therefore this is just a functional component that returns an input field with some args,
+that we will alter inside the App component in App.js.
+
+the onChange is a react tag and its value is passed from the components props as handleChange for example.
+
+This handleChange is passed to the SearchBox component inside other components like as in App component.
+After than the state changing operation is attached to the onChange which in the end converted to an onchange HTML element*/
