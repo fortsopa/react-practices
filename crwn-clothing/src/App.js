fe00ce46f14d665b882fd69bea60a08c40330b91
KdotA <kenan@akinci.io>
2022-01-17 21:04:05 +0100

    Added react-router-dom

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './components/homepage.component';
 import './App.css';
 
+const TopicsList = () => (
+  <div>
+    <h1>Topic List Page</h1>
+  </div>
+);
+
+const TopicDetail = () => (
+  <div>
+    <h1>Topic Detail Page</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/topics" component={TopicsList}></Route>
+        <Route path="/topics/:topicId" component={TopicDetail}></Route>
+      </Switch>
     </div>
   );
 }
