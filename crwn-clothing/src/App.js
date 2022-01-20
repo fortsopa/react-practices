34a510706b34918c03e3908270a982bf806de6b7
KdotA <kenan@akinci.io>
2022-01-20 23:19:17 +0100

    Shop page added, collection-preview component, collection-item component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,36 +1,16 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
-
-import HomePage from './components/homepage.component';
 import './App.css';
 
-const TopicsList = props => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Topic List Page</h1>
-    </div>
-  );
-};
-
-const TopicDetail = props => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Topic Detail Page: {props.match.params.topicId}</h1>
-    </div>
-  );
-};
-
-// :topicId -> is a url parameter
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/topics" component={TopicsList} />
-        <Route path="/topics/:topicId" component={TopicDetail} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
