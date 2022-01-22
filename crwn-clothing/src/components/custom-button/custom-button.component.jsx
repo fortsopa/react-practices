89af48d1ea9cc95ae2deae5ad709482c19142ba5
KdotA <kenan@akinci.io>
2022-01-22 01:28:41 +0100

    Small fixes

--- crwn-clothing/src/components/custom-button/custom-button.component.jsx
+++ crwn-clothing/src/components/custom-button/custom-button.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, ...otherProps }) => [
+const CustomButton = ({ children, ...otherProps }) => (
   <button className="custom-button" {...otherProps}>
     {children}
-  </button>,
-];
+  </button>
+);
 
 export default CustomButton;
